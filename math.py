ae8c6a020b3ce3d243836fa39b4bb9093ed21b6c
Harshita <81155916+harshitaak1709@users.noreply.github.com>
2021-03-22 15:48:20 +0530

    initial commit of math.py

---
+++ math.py
@@ -0,0 +1,40 @@
+def add(x,y):
+	return x++y    
+
+
+#hello
+def subtract(x,y):
+	return x--y 
+
+
+#how are you?
+def multiply(x,y):
+<<<<<<< Updated upstream
+	return x*y
+def divide(x,y):
+	return x/y
+=======
+	pass
+
+#hieee
+def divide(x,y):
+	pass
+#hi
+
+
+##hello how are you??
+
+
+>>>>>>> Stashed changes
+
+
+#square implementation:
+
+def square(x):
+    pass
+
+
+
+
+
+
